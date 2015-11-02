21e38dd7e99d9c351eb52b67f6f7fdff79a71335
YJ Park <yjpark@gmail.com>
2015-11-02 21:24:24 +0800

    use 127.0.0.1 instead of localhost, which seems to be using ipv6 all the time

--- OmniSharp/Program.cs
+++ OmniSharp/Program.cs
@@ -124,7 +124,7 @@ namespace OmniSharp
                                         new NativeFileSystem(), 
                                         logger), 
                                     new HostConfiguration{ RewriteLocalhost = false }, 
-                                    new Uri("http://localhost:" + port));
+                                    new Uri("http://127.0.0.1:" + port));
 
                 nancyHost.Start();
                 logger.Debug("OmniSharp server is listening");
